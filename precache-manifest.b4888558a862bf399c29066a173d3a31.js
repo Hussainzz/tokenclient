a8e740ce69da65515eff19013c28fb78bea2d646
Hussainzz <shkhussain12@gmail.com>
2022-02-02 07:35:50 +0530

    Updates

---
+++ precache-manifest.b4888558a862bf399c29066a173d3a31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18df694a28a4bc7d02cc092b7af5e54f",
+    "url": "/tokenclientweb3/index.html"
+  },
+  {
+    "revision": "af23946bb210b08af32f",
+    "url": "/tokenclientweb3/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fef90b0027cce6922486",
+    "url": "/tokenclientweb3/static/js/2.edd9a37b.chunk.js"
+  },
+  {
+    "revision": "af23946bb210b08af32f",
+    "url": "/tokenclientweb3/static/js/main.3ba64425.chunk.js"
+  },
+  {
+    "revision": "078b2bb357750f22d76e",
+    "url": "/tokenclientweb3/static/js/runtime-main.1290f812.js"
+  }
+]);
